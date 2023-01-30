72c15cb5cc32df0cb2f96d82cf6dbcd9c6d31bee
PatilAnkita07 <123736728+PatilAnkita07@users.noreply.github.com>
2023-01-30 03:59:57 -0800

    Initial program
    Demo

---
+++ Demoproject/src/myPackage/Pragram1.java
@@ -0,0 +1,12 @@
+package myPackage;
+
+public class Pragram1 {
+	public static void main(String[] args)
+	{
+		System.out.println("Running Program1");
+		System.out.println("Running Program1 on Git");
+		System.out.println("Running Program1 on Github");
+		
+	}
+
+}
